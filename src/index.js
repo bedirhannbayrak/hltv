38e9edb95befe0eee6ca0a05c08ced218acbc8af
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-01-23 01:06:21 +0300

    jsx,first class,child props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import Card from './components/Card'
+import Collapse from './components/Collapse'
 
+
+
+function App() {
+    return (
+        <div>
+
+            <div className='card-group'>
+                <Collapse href='collapseExample1'>
+                    <Card
+                        cardTitle='Beşiktaş'
+                        cardText='lorem ipsum text I'
+                        updateTime='3 minutes ago'
+                        image='https://picsum.photos/id/1/200/300'
+                    />
+                </Collapse>
+                <Collapse href='collapseExample2'>
+                    <Card
+                        cardTitle='Gala'
+                        cardText='lorem ipsum text II'
+                        updateTime='3 minutes ago'
+                        image='https://picsum.photos/id/2/200/300'
+                    />
+                </Collapse>
+                <Collapse href='collapseExample3'>
+                    <Card
+                        cardTitle='fener'
+                        cardText='lorem ipsum text III'
+                        updateTime='3 minutes ago'
+                        image='https://picsum.photos/id/5/200/300'
+                    />
+                </Collapse>
+
+            </div>
+
+
+        </div>
+    )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
