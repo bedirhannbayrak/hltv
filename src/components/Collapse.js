f7ab30efe6be650df34a6e6f7b867667ce036031
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-01-24 16:02:50 +0300

    setState ve Binding

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,15 +1,45 @@
 import React from 'react';
 
 class Collapse extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showContent: false
+            };   
+        // this.showMore = this.showMore.bind(this)   this'in showMore() fonskiyonunda tanımlı olabililmesi için JS'in bind methodunu kullandık
+        // bind yöntemi de genelde kullanılan bir yöntem değildir.
+        // genelde arrow function yöntemi
+    }
+
+/*    showMore() {
+        //console.log(this)
+        // this.setState ({ showContent: true}) burada showMore methodunu kendimiz oluşturduğumuz için "this" undefined dönüyor.
+        // render veya constructor methodları önceden tanımlı olduğu için console.log(this) yaptığımızda undefined dönmüyor
+        this.setState({showContent:true})
+    }
+    */
+
+    showMore = () => {
+        // console.log(this)
+        this.setState({showContent: !this.state.showContent})
+    }
     render () {
         return (
             <div>
-                    <a className="btn btn-primary w-100" data-bs-toggle="collapse" href={"#".concat(this.props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <button className="btn btn-primary w-100" onClick={this.showMore}>
                         Link with href
-                    </a>          
-                <div className="collapse show" id={this.props.href}>
+                    </button>   
+            {
+                this.state.showContent ? (
+                    <div className="collapse show">
                     {this.props.children}
                 </div>
+
+                ) : 
+                null
+            }       
+                
             </div>
         );
     }
