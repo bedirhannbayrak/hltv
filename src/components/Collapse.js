dd3c9c9c1a8f47b3567d1f7767b4654ef7051bdf
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-01-24 17:34:28 +0300

    state,props, yaşam döngüs

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,50 +1,68 @@
 import React from 'react';
 
 class Collapse extends React.Component {
-    constructor() {
-        super();
+    // constructor() {
+    //     console.log('constractor calisti')
+    //     super();
 
-        this.state = {
-            showContent: false
-            };   
-        // this.showMore = this.showMore.bind(this)   this'in showMore() fonskiyonunda tanımlı olabililmesi için JS'in bind methodunu kullandık
-        // bind yöntemi de genelde kullanılan bir yöntem değildir.
-        // genelde arrow function yöntemi
-    }
+    //     this.state = {
+    //         showContent: false
+    //     };
+    //     // this.showMore = this.showMore.bind(this)   this'in showMore() fonskiyonunda tanımlı olabililmesi için JS'in bind methodunu kullandık
+    //     // bind yöntemi de genelde kullanılan bir yöntem değildir.
+    //     // genelde arrow function yöntemi
+    // }
+    
+    // yukarıdaki constructor methodunda tanımladığımız state'i aşağıdaki şekilde de tanımlayabiliriz
+    state= {showContent:false}
 
-/*    showMore() {
-        //console.log(this)
-        // this.setState ({ showContent: true}) burada showMore methodunu kendimiz oluşturduğumuz için "this" undefined dönüyor.
-        // render veya constructor methodları önceden tanımlı olduğu için console.log(this) yaptığımızda undefined dönmüyor
-        this.setState({showContent:true})
-    }
-    */
+    /*    showMore() {
+            //console.log(this)
+            // this.setState ({ showContent: true}) burada showMore methodunu kendimiz oluşturduğumuz için "this" undefined dönüyor.
+            // render veya constructor methodları önceden tanımlı olduğu için console.log(this) yaptığımızda undefined dönmüyor
+            this.setState({showContent:true})
+        }
+        */
 
     showMore = () => {
         // console.log(this)
-        this.setState({showContent: !this.state.showContent})
+        this.setState({ showContent: !this.state.showContent })
     }
-    render () {
-        return (
-            <div>
-                    <button className="btn btn-primary w-100" onClick={this.showMore}>
-                        Link with href
-                    </button>   
-            {
-                this.state.showContent ? (
-                    <div className="collapse show">
-                    {this.props.children}
-                </div>
 
-                ) : 
-                null
-            }       
-                
+    componentDidMount(){
+        console.log('component did mount calisti')
+    }
+
+    componentDidUpdate(){
+        console.log('componentdidupdate calisti')
+    }
+    render() {
+        return (
+            console.log('render calisti'),
+            <div>
+                <button className="btn btn-primary w-100" onClick={this.showMore}>
+                    {/* {this.props.children.props.cardTitle} bunu kaldırarak aşağıdaki kodu yazdık daha kullanışlıdır
+                    genel kullanımı böyledir. */}
+
+                    {React.Children.map(this.props.children, children => children.props.cardTitle)}
+
+                    </button>
+                {
+                    this.state.showContent ? (
+                        <div className="collapse show">
+                            {/* {this.props.children} komutu yerine aşağıdaki kodu yazdık*/}
+                            {React.Children.map(this.props.children, children=>children)}
+                        </div>
+
+                    ) :
+                        null
+                }
+
             </div>
         );
     }
 };
-    
+
 
 
 
